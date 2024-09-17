ed2155eca39c50474b7956806ec5019bbcc3d320
ktinson <monikai.tinson@verizon.com>
2024-09-17 08:50:34 -0500

    Added associations

--- models/index.js
+++ models/index.js
@@ -3,7 +3,16 @@ const Like = require("./Like");
 const Post = require("./Post");
 const Profile = require("./Profile");
 const User = require("./User");
+User.hasOne(Profile)
+Profile.belongsTo(User)
 
+User.hasMany(Post)
+Post.belongsTo(User)
+
+Post.hasMany(Comment)
+Comment.belongsTo(Post)
+User.belongsToMany(Like, {through: "User-Like"})
+Like.belongsToMany(User, {through: "User-Like"})
 module.exports = {
     Comment,
     Like,
